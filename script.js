f6e89a2ba470943925baf21dc6274ad7712dceeb
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2026-01-18 14:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //your JS code here. If required.
 function removeColor() {
 	let select = document.getElementById("colorSelect");
-	select.remove(select.selelctedIndex)
+	let selectedOption = select.selectedIndex;
+
+	if (selectedOption !== -1) {
+		select.remove(selectedOption);
+	}
 }
